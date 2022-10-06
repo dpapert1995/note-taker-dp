0959675b56299faef96b28bcfda197ea984a2274
Daniel Papert <dpapert@gmail.com>
2022-10-06 13:15:23 -0400

    Set up middleware, routes, and global variables.

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const express = require("express");
 const fs = require("fs");
 const path = require('path');
 
+// Set up middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Setup global notes variable for back end
+fs.readFile("./db/db.json","json",function(data) {
+    var notes = data;
+});
+
+// Require routes files for HTML and API
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
 // Initialize express app
 const app = express();
 const PORT = process.env.PORT || 3001;
