1cc33bbc47831b078adacef3b1bc85140a6e3eab
Daniel Papert <dpapert@gmail.com>
2022-10-10 22:37:55 -0400

    Forgot to install moment. Added sendFile requests. Wrote view routes.

---
+++ routes/apiRoutes.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const moment = require("moment");
+
+module.exports = app => {
+
+    var readFileAsync = util.promisify(fs.readFile);
+
+    // Setup notes variable
+    readFileAsync("db/db.json","utf8").then(function(data) {
+        var notes = JSON.parse(data);
+        var noteCount = notes.length;
+
+    // Read the db.json file and return all saved notes as JSON.
+    app.get("/api/notes", function(req, res) {
+        res.json(notes);
+    });
+
+    // Receives a new note, adds it to db.json, then returns the new note
+    app.post("/api/notes", function(req, res) {
+        let noteNew = req.body;
+        notes[noteCount++] = noteNew;
+        updateDataBase();
+        return console.log("Added new note: "+ noteNew.title);
+    });
+
+    // Retrieves a note with specific id
+    app.get("/api/notes/:id", function(req,res) {
+        res.json(notes[req.params.id]);
+    });
+
+     // Deletes a note at a specific index
+     app.delete("/api/notes/:id", function(req, res) {
+        delete notes[req.params.id];
+        updateDataBase();
+        console.log("Deleting note at index "+req.params.id);
+    });
+
+    // Sends note to note.html
+    app.get('/notes', function(req,res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    // Sends updated notes to index.html
+    app.get('*', function(req,res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    // Updates database with new note
+    function updateDataBase() {
+        fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
+            if (err) return console.log(err);
+            return true;
+        });
+        }
+    })
+}
--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 // Require Dependencies
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 const path = require('path');
+
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Set up middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Setup global notes variable for back end
-fs.readFile("./db/db.json","json",function(data) {
-    var notes = data;
-});
-
-var noteCount = 0;
-
 // Require routes files for HTML and API
-require('./routes/htmlRoutes')(app);
-require('./routes/apiRoutes')(app);
-
-// Initialize express app
-const app = express();
-const PORT = process.env.PORT || 3001;
+require('./routes/apiRoutes.js')(app);
 
 // Setup listener
 app.listen(PORT, function() {
